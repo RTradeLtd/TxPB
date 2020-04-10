edf7f9b2547421e7340cc47b964ef5136071bf3e
postables <postables@rtradetechnologies.com>
2020-04-09 21:04:41 -0700

    add missing blockstore calls

--- js/node_pb.js
+++ js/node_pb.js
@@ -4877,7 +4877,9 @@ proto.pb.BSREQTYPE = {
   BS_GET: 3,
   BS_GET_MANY: 4,
   BS_GET_ALL: 5,
-  BS_GET_STATS: 6
+  BS_GET_STATS: 6,
+  BS_HAS: 7,
+  BS_HASH_ON_READ: 8
 };
 
 /**
--- ts/node_pb.d.ts
+++ ts/node_pb.d.ts
@@ -698,6 +698,8 @@ export interface BSREQTYPEMap {
   BS_GET_MANY: 4;
   BS_GET_ALL: 5;
   BS_GET_STATS: 6;
+  BS_HAS: 7;
+  BS_HASH_ON_READ: 8;
 }
 
 export const BSREQTYPE: BSREQTYPEMap;
