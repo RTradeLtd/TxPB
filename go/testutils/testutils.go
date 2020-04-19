fa330ca5cdff62bf3b9e8ec1aa516f7a5175a83e
postables <postables@rtradetechnologies.com>
2020-04-19 16:16:38 -0700

    remove dependency on sdk client

--- go/testutils/testutils.go
+++ go/testutils/testutils.go
@@ -3,21 +3,7 @@ package testutils
 import (
 	"os"
 	"testing"
-
-	sdkc "github.com/RTradeLtd/go-temporalx-sdk/client"
 )
-
-// GetClient returns an initialized sdk client
-func GetClient(t *testing.T) *sdkc.Client {
-	client, err := sdkc.NewClient(sdkc.Opts{
-		ListenAddress: GetXAPIAddress(t),
-		Insecure:      GetXAPISecure(t),
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	return client
-}
 
 // GetXAPIAddress returns the address to used to talk to TemporalX
 func GetXAPIAddress(t *testing.T) string {
--- go/testutils/testutils_test.go
+++ go/testutils/testutils_test.go
@@ -5,12 +5,26 @@ import (
 	"testing"
 )
 
-func TestUtils(t *testing.T) {
-	client := GetClient(t)
-	client.Close()
-	if err := os.Setenv("TEST_XAPI", "xapi.temporal.cloud:9090"); err != nil {
+func TestGetXAPIAddress(t *testing.T) {
+	if addr := GetXAPIAddress(t); addr != "xapi.temporal.cloud:9090" {
+		t.Fatal("bad address")
+	}
+	if err := os.Setenv("TEST_XAPI", "xxapi.temporal.cloud:9090"); err != nil {
 		t.Fatal(err)
 	}
-	client = GetClient(t)
-	client.Close()
+	if addr := GetXAPIAddress(t); addr != "xxapi.temporal.cloud:9090" {
+		t.Fatal("bad address")
+	}
+}
+
+func TestXAPISecure(t *testing.T) {
+	if GetXAPISecure(t) {
+		t.Fatal("should be false")
+	}
+	if err := os.Setenv("TEST_XAPI_SECURE", "true"); err != nil {
+		t.Fatal(err)
+	}
+	if !GetXAPISecure(t) {
+		t.Fatal("should be true")
+	}
 }
